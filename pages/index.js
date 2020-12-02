8e93eb1f93ceb1aeb8fd1526c085e6e23d48bf0f
GabrielPT3 <gjcn2002@gmail.com>
2020-12-02 11:22:05 +0000

    nova Homepage

--- pages/index.js
+++ pages/index.js
@@ -4,25 +4,10 @@ function Home(){
     return (
         <div>
             <h2>Home</h2>
-            <p>Adicionar Contador</p>
-            <Contador></Contador>
         </div>
     );
 }
 
-function Contador(){
-    const [contador, setContador] = useState(1);
 
-    function adicionarContador(){
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            {contador}
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    );
-}
 
 export default Home;
