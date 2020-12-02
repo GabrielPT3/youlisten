eeef5f03c8dcee6a7707731694f4bab6f57211f4
GabrielPT3 <gjcn2002@gmail.com>
2020-12-02 11:45:28 +0000

    update homepage

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import { useState } from 'react';
 
+
 function Home(){
     return (
-        <div>
-            <h2>Home</h2>
+        <div className="navbar">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Music</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
         </div>
     );
 }
